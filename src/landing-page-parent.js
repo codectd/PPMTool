23549fad3e6ef7fadb6d16f604af22d37c6f8788
William Thompson <williamthompson@Williams-MacBook-Pro.local>
2021-01-26 13:58:50 -0500

    initial boilerplate

---
+++ src/landing-page-parent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LandingPageParent = (props) => {
+    return (
+        <div>
+            landing page
+        </div>
+    );
+};
+
+export default LandingPageParent;
